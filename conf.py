ad15cfacf04cb2d521dc916ec6872b3711fba43e
Mastan Vali P <mastanvali.apssdc@gmail.com>
2020-11-26 15:29:07 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'E-commerce project using Java'
-copyright = '2020, Harideep'
-author = 'Harideep'
+project = 'LMS Project'
+copyright = '2020, Mastan Vali'
+author = 'Mastan Vali'
 
 # The full version, including alpha/beta/rc tags
 release = ''
